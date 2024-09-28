1a94ac84b414f956257d267d65cc3a21f6df7008
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-09-28 21:54:22 +0600

    update member_dto

--- application/boot/boot/dto/member_dto.py
+++ application/boot/boot/dto/member_dto.py
@@ -2,27 +2,29 @@ from boot.model.member import Member
 from pweb_form_rest import fields, PWebRestDTO
 
 
-class PersonDetailsDTO(PWebRestDTO):
+class MemberDetailsDTO(PWebRestDTO):
     class Meta:
-        model = Person
+        model = Member
         load_instance = True
 
     name = fields.String(required=True, error_messages={"required": "Please enter name"})
+    roll = fields.String(required=True, error_messages={"required": "Please enter roll"})
+    registration = fields.String(required=True, error_messages={"required": "Please enter registration"})
     email = fields.Email(required=True, error_messages={"required": "Please enter email"})
     address = fields.String(allow_none=True)
 
 
-class PersonCreateDTO(PersonDetailsDTO):
+class MemberCreateDTO(MemberDetailsDTO):
     class Meta:
-        model = Person
+        model = Member
         load_instance = True
 
     password = fields.String(required=True, error_messages={"required": "Please enter password"})
 
 
-class PersonUpdateDTO(PersonDetailsDTO):
+class MemberUpdateDTO(MemberDetailsDTO):
     class Meta:
-        model = Person
+        model = Member
         load_instance = True
 
     id = fields.Integer(required=True, error_messages={"required": "Please enter id"})
